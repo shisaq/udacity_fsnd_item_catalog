30993ecf4fd89249a6443993a6ddae1be278ede2
shisaq <shisaq@gmail.com>
2017-10-08 15:58:57 -0400

    feat: add html templates and config different actions by using fake data

--- application.py
+++ application.py
@@ -1,31 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+# fake database
+category = {'name': 'haha1', 'id': '1'}
+categories = [
+    {'name': 'haha1', 'id': '1'},
+    {'name': 'haha2', 'id': '2'}
+]
+item = {'name': 'item1', 'description': 'wowowowowowowo1', 'course': 'haha1', 'id': '1'}
+items = [
+    {'name': 'item1', 'description': 'wowowowowowowo1', 'course': 'haha1', 'id': '1'},
+    {'name': 'item2', 'description': 'wowowowowowowo2', 'course': 'haha2', 'id': '2'}
+]
 
 @app.route('/')
 @app.route('/catalog')
 def homepage():
-    return 'homepage'
+    return render_template('homepage.html', categories = categories, items = items)
 
 @app.route('/catalog/<category_name>/items')
-def allCategoryItems(category_name):
-    return category_name
+def itemsList(category_name):
+    return render_template('itemsList.html', categories = categories, category = category, items = items)
 
 @app.route('/catalog/<category_name>/<item_name>')
 def showItem(category_name, item_name):
-    return '%s, %s' % (category_name, item_name)
+    return render_template('itemInfo.html', item = item)
 
 @app.route('/catalog/add')
 def addItem():
-    return 'add item'
+    return render_template('addItem.html', categories = categories)
 
 @app.route('/catalog/<item_name>/edit')
 def editItem(item_name):
-    return 'edit item'
+    return render_template('editItem.html', categories = categories, item = item)
 
 @app.route('/catalog/<item_name>/delete')
 def deleteItem(item_name):
-    return 'delete item'
+    return render_template('deleteItem.html', item = item)
 
 
 if __name__ == '__main__':
