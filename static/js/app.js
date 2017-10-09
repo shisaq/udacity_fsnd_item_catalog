58af09a1619bc606ea450a64d8c76b3f5a58a3e3
shisaq <shisaq@gmail.com>
2017-10-09 11:17:45 -0400

    feat: add typed.js and webpack!

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,13 @@
+import Typed from 'typed.js';
+
 // load particles.js
 particlesJS.load('particles-js', 'static/js/particles.config.json', function() {
   console.log('callback - particles.js config loaded');
 });
+
+var options = {
+  strings: ["<i>First</i> sentence.", "&amp; a second sentence."],
+  typeSpeed: 40
+}
+
+var typed = new Typed(".intro", options);
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  context: path.join(__dirname, "static"),
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: "./js/app.js",
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env']
+          // presets: ['react', 'es2015', 'stage-0'],
+          // plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties']
+        }
+      }
+    ]
+  },
+  output: {
+    path: __dirname + "/static/js/",
+    filename: "app.min.js"
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ]
+};
