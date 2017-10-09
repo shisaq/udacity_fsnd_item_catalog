f0bdba9024b175ace1e3969f22234eba3ee00c60
shisaq <shisaq@gmail.com>
2017-10-09 12:23:19 -0400

    css: customize welcome typed.js section

--- static/js/app.js
+++ static/js/app.js
@@ -8,8 +8,17 @@ window.particlesJS.load('particles-js', 'static/js/particles.config.json')
 // load typed.js
 // https://github.com/mattboldt/typed.js/
 var options = {
-  strings: ["<i>First</i> sentence.", "&amp; a second sentence."],
-  typeSpeed: 40
+  strings: [
+    "a full stack developer.",
+    "a front end developer.",
+    "a singer.",
+    "a videographer.",
+    "a UX designer.",
+    "a writer.",
+  ],
+  typeSpeed: 50,
+  loop: true,
+  showCursor: false
 }
 
 var typed = new Typed(".intro", options);
